f7c3a1ed37856c5383077e7bcdb15e6bcccd8efd
surya-mulkewar-94 <suryakant@simplified.co>
2025-05-24 13:46:44 +0530

    Recaptch Changes

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-function beforesubmit(){
-    let outputdate = document.querySelector('.outputdate');
-    let inputdate = document.querySelector('.inputdate');
+function beforesubmit() {
+  let outputdate = document.querySelector(".outputdate");
+  let inputdate = document.querySelector(".inputdate");
 
-    console.log('inputdate.value', inputdate.value); // script ==> date(en_IN)
+  console.log("inputdate.value", inputdate.value); // script ==> date(en_IN)
 
-    let formatteddate = new Date(inputdate.value).toLocaleDateString('en-IN');
+  let formatteddate = new Date(inputdate.value).toLocaleDateString("en-IN");
 
-    console.log(formatteddate);
+  console.log(formatteddate);
 
-    outputdate.value = formatteddate;
-    
-    
+  outputdate.value = formatteddate;
 }
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
