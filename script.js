70a93c0fb84243e79d218e13b0ef475b60d65847
surya-mulkewar-94 <suryakant@simplified.co>
2025-05-24 14:01:59 +0530

    reCAPTCHA validations

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-function beforesubmit() {
-  let outputdate = document.querySelector(".outputdate");
-  let inputdate = document.querySelector(".inputdate");
+let captchaChecked = false;
+function beforesubmit(event) {
 
-  console.log("inputdate.value", inputdate.value); // script ==> date(en_IN)
+    if (captchaChecked) {
+        let outputdate = document.querySelector(".outputdate");
+        let inputdate = document.querySelector(".inputdate");
 
-  let formatteddate = new Date(inputdate.value).toLocaleDateString("en-IN");
+        console.log("inputdate.value", inputdate.value); // script ==> date(en_IN)
 
-  console.log(formatteddate);
+        let formatteddate = new Date(inputdate.value).toLocaleDateString("en-IN");
 
-  outputdate.value = formatteddate;
+        console.log(formatteddate);
+
+        outputdate.value = formatteddate;
+    }
+    else{
+        alert('Please check the reCAPTCHA box to submit the lead');
+        event.preventDefault();
+    }
+
 }
 
 function timestamp() {
-  var response = document.getElementById("g-recaptcha-response");
-  if (response == null || response.value.trim() == "") {
-    var elems = JSON.parse(
-      document.getElementsByName("captcha_settings")[0].value
-    );
-    elems["ts"] = JSON.stringify(new Date().getTime());
-    document.getElementsByName("captcha_settings")[0].value =
-      JSON.stringify(elems);
-  }
+    var response = document.getElementById("g-recaptcha-response");
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(
+            document.getElementsByName("captcha_settings")[0].value
+        );
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value =
+            JSON.stringify(elems);
+    }
 }
 setInterval(timestamp, 500);
+
+captchasuccess(){
+    captchaChecked = true;
+}
